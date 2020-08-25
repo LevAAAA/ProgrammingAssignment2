aff8337cd224bab2adaf5cdc8e3bdac6e8f1dc14
Andriy Levytskyy <leva@leva.ca>
2020-08-25 15:18:08 -0400

    Completed functions for assigment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
+## functions to improve computation time for repeated calculation of inverse
+## using caching mechanism to store matrix
 
-## Write a short comment describing this function
+
+## function creating object with properties to set/get values/properties 
 
 makeCacheMatrix <- function(x = matrix()) {
+  i <- NULL
+  set <- function(y) {
+    x <<- y
+    i <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) i <<- inverse
+  getinverse <- function() i
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)  
 
 }
 
 
-## Write a short comment describing this function
+## create inverse matrix and get inverse matrix from cache if already exist
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  i <- x$getinverse()
+  if(is.null(i)){
+    data <- x$get()
+    i <- solve(data, ...)
+    x$setinverse(i)
+  }
+  i
 }
